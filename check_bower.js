7b61fa3b74bd70a713aea9df512184b960d67331
Eric Stout <eric@factor1studios.com>
2015-10-29 21:32:04 -0700

    Adjusts bower path in check_bower.js

--- check_bower.js
+++ check_bower.js
@@ -1,7 +1,7 @@
 var bowerJson = require('bower-json');
 
 // Can also be used by simply calling bowerJson()
-bowerJson.read('./bower.json', function (err, json) {
+bowerJson.read('bower.json', function (err, json) {
     if (err) {
         console.error('There was an error reading the file');
         console.error(err.message);
